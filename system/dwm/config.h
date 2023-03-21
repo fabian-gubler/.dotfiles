6273dc44a9e7a3f42692e5cfc3b503f18941b7d5
Fabian Gubler <fabian.gubler@hotmail.com>
2023-03-21 19:53:40 +0100

    automated update

--- system/dwm/config.h
+++ system/dwm/config.h
@@ -41,12 +41,10 @@ static const Rule rules[] = {
 	 */
 	/* class				instance    title       tags mask   isfloating   monitor    floatborderpx*/
 	{ "trayer",				NULL,       NULL,       ~0,			0,			 -1,		0 },
-	// { "Chromium-browser",	NULL,       NULL,       1 << 4,		0,           -1,        0 },
 	{ "firefox",			NULL,       NULL,       1 << 3,		0,           -1,        0 },
 	{ "Com.github.johnfactotum.Foliate",NULL, NULL, 1 << 2,		0,           -1,        0 },
 	{ "okular",				NULL,       NULL,       1 << 2,		0,           -1,        0 },
 	{ "mpv",				NULL,       NULL,       1 << 2,		0,           -1,        0 },
-	// { "qutebrowser",		NULL,       NULL,       1 << 1,		0,           -1,        0 },
 	{ "kitty",				NULL,       NULL,       0,			0,           -1,        0 },
 
 	/* class				instance    title       tags mask   isfloating   monitor    float x,y,w,h       floatborderpx*/
