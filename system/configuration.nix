1f2b747f8cfc2afc6a3515dc65e176e4b5ec5849
Fabian Gubler <fabian.gubler@hotmail.com>
2023-03-29 08:02:22 +0200

    automated update

--- system/configuration.nix
+++ system/configuration.nix
@@ -229,24 +229,24 @@ in
 
   };
 
-  services.mysql = {
-	package = pkgs.mariadb;
-    enable = true;
-  };
+ #  services.mysql = {
+	# package = pkgs.mariadb;
+ #    enable = true;
+ #  };
 
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_13;
-    enableTCPIP = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      local all all trust
-      host all all 127.0.0.1/32 trust
-      host all all ::1/128 trust
-    '';
-    initialScript = pkgs.writeText "backend-initScript" ''
-	  CREATE ROLE fabian WITH LOGIN CREATEDB PASSWORD 'your_password';
-    CREATE DATABASE your_database WITH OWNER fabian;
-      GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
-    '';
-  };
+  # services.postgresql = {
+  #   enable = true;
+  #   package = pkgs.postgresql_13;
+  #   enableTCPIP = true;
+  #   authentication = pkgs.lib.mkOverride 10 ''
+  #     local all all trust
+  #     host all all 127.0.0.1/32 trust
+  #     host all all ::1/128 trust
+  #   '';
+  #   initialScript = pkgs.writeText "backend-initScript" ''
+	 #  CREATE ROLE fabian WITH LOGIN CREATEDB PASSWORD 'your_password';
+  #   CREATE DATABASE your_database WITH OWNER fabian;
+  #     GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+  #   '';
+  # };
 }
