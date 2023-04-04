e8331f659bd35b3113b73726ea08dcba404cbd67
Fabian Gubler <fabian.gubler@hotmail.com>
2023-04-05 00:49:27 +0200

    automated update

--- system/configuration.nix
+++ system/configuration.nix
@@ -45,17 +45,19 @@ in
 
   # Set your time zone and locals
   time.timeZone = "Europe/Zurich";
-
   # Display Server
   services.xserver = {
     enable = true;
     layout = "ch";
+    # modules = [ pkgs.xf86_input_wacom ];
+    # libinput.enable = true;
+    # wacom.enable = true;
     # windowManager.dwm.enable = true;
-    desktopManager.gnome.enable = true;
-    displayManager = {
-      # defaultSession = "none+dwm";
-      gdm.enable = true;
-    };
+    # desktopManager.gnome.enable = true;
+    # displayManager = {
+    #   # defaultSession = "none+dwm";
+    #   # gdm.enable = true;
+    # };
     serverFlagsSection = ''
       Option "BlankTime" "0"
       Option "StandbyTime" "0"
@@ -64,117 +66,126 @@ in
     '';
   };
 
-  # Flatpak
-  # xdg.portal.enable = true;
-  # services.flatpak.enable = true;
 
-  # TODO: System-wide GTK Theme
-  qt5 = {
-    enable = true;
-    platformTheme = "gtk2";
-    style = "gtk2";
-  };
-
-  networking = {
-    hostName = "nixos";
-    # hosts = {
-    #   "0.0.0.0" = [ "youtube.com" "www.youtube.com" ];
-    # };
-    networkmanager.enable = true;
-    stevenBlackHosts = {
-      blockFakenews = true;
-      blockGambling = true;
-      blockPorn = true;
-      blockSocial = false;
+  virtualisation.virtualbox.guest.enable = true;
+  services.xserver =
+    {
+      desktopManager.default = "none";
+      windowManager.default = "i3";
+      windowManager.i3.enable = true;
+      displayManager.lightdm.enable = true;
     };
-  };
+      # Flatpak
+      # xdg.portal.enable = true;
+      # services.flatpak.enable = true;
 
-  # Use same keyboard layout for tty
-  console.useXkbConfig = true;
+      # TODO: System-wide GTK Theme
+      qt5 = {
+  enable = true;
+  platformTheme = "gtk2";
+  style = "gtk2";
+};
 
-  # Pipewire
-  # services = {
-  #   pipewire = {
-  #     enable = true;
-  #     alsa = {
-  #       enable = true;
-  #       support32Bit = true;
-  #     };
-  #     pulse.enable = true;
-  #     jack.enable = true;
-  #   };
-  # };
+networking = {
+hostName = "nixos";
+# hosts = {
+#   "0.0.0.0" = [ "youtube.com" "www.youtube.com" ];
+# };
+networkmanager.enable = true;
+stevenBlackHosts = {
+blockFakenews = true;
+blockGambling = true;
+blockPorn = true;
+blockSocial = false;
+};
+};
 
-  # Service management
-  # services.logind.extraConfig = ''
-  #   # don’t shutdown when power button is short-pressed
-  #   HandlePowerKey=ignore
-  # '';
+# Use same keyboard layout for tty
+console.useXkbConfig = true;
 
-  # Bluetooth
-  # hardware = {
-  #   keyboard.zsa.enable = true;
-  #   bluetooth = {
-  #     enable = true;
-  #     settings = {
-  #       General = {
-  #         Enable = "Source,Sink,Media,Socket";
-  #       };
-  #     };
-  #   };
-  # };
+# Pipewire
+# services = {
+#   pipewire = {
+#     enable = true;
+#     alsa = {
+#       enable = true;
+#       support32Bit = true;
+#     };
+#     pulse.enable = true;
+#     jack.enable = true;
+#   };
+# };
 
-  services = {
-    # touchegg.enable = true;
-    # gnome.gnome-keyring.enable = true;
-    # tlp.enable = true;
-    atd.enable = true;
-    # unclutter = {
-    #   enable = true;
-    #   timeout = 1;
-    # };
+# Service management
+# services.logind.extraConfig = ''
+#   # don’t shutdown when power button is short-pressed
+#   HandlePowerKey=ignore
+# '';
 
+# Bluetooth
+# hardware = {
+#   keyboard.zsa.enable = true;
+#   bluetooth = {
+#     enable = true;
+#     settings = {
+#       General = {
+#         Enable = "Source,Sink,Media,Socket";
+#       };
+#     };
+#   };
+# };
 
-    # redshift = {
-    #   enable = true;
-    #   temperature.day = 6500;
-    #   temperature.night = 3500;
-    #   brightness.day = "1";
-    #   brightness.night = "0.7";
-    # };
-
-
-  };
-
-  # unlock gnome keyring automatically
-  # security.pam.services.startx.enableGnomeKeyring = true;
-
-  # virtualisation
-
-  # boot.kernelModules = [ "kvm-intel" "kvmfr" ];
-  # boot.kernelParams = [ "amd_iommu=on" "pcie_aspm=off" ];
-
-
-  fonts.fonts = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-  ];
+services = {
+# touchegg.enable = true;
+# gnome.gnome-keyring.enable = true;
+# tlp.enable = true;
+atd.enable = true;
+# unclutter = {
+#   enable = true;
+#   timeout = 1;
+# };
 
 
-  programs = {
-    tmux.enable = true;
-    # slock.enable = true;
-    seahorse.enable = true;
-    java.enable = true;
-    dconf.enable = true;
-
-    firefox = {
-      enable = true;
-      # TODO: Debug 
-      preferences = {
-        "font.size.systemFontScale" = 130;
-      };
-    };
+# redshift = {
+#   enable = true;
+#   temperature.day = 6500;
+#   temperature.night = 3500;
+#   brightness.day = "1";
+#   brightness.night = "0.7";
+# };
 
 
-  };
+};
+
+# unlock gnome keyring automatically
+# security.pam.services.startx.enableGnomeKeyring = true;
+
+# virtualisation
+
+# boot.kernelModules = [ "kvm-intel" "kvmfr" ];
+# boot.kernelParams = [ "amd_iommu=on" "pcie_aspm=off" ];
+
+
+fonts.fonts = with pkgs; [
+(nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+];
+
+
+programs = {
+tmux.enable = true;
+# slock.enable = true;
+seahorse.enable = true;
+java.enable = true;
+dconf.enable = true;
+
+firefox = {
+enable = true;
+# TODO: Debug 
+preferences = {
+"font.size.systemFontScale" = 130;
+};
+};
+
+
+};
 }
