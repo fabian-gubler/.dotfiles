b077542ef79066a9fb8a066fc997cc08fa80d427
Fabian Gubler <fabian.gubler@hotmail.com>
2023-04-07 20:04:18 +0200

    manual update

--- modules/default.nix
+++ modules/default.nix
@@ -1,3 +1,53 @@
 { config, pkgs, ... }: {
-  imports = [ ./home.nix ];
+
+  # Let Home Manager install and manage itself.
+  home.stateVersion = "22.11";
+  programs.home-manager.enable = true;
+
+  imports = [
+    ./files
+    ./programs
+  ];
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs;
+    [ notmuch-mutt notmuch lynx ] ++
+    [ khal khard vdirsyncer inotify-tools ] ++
+    [
+      neomutt
+      dunst
+      autorandr
+      spicetify-cli
+      newsboat
+	  signal-desktop 
+	  teams 
+	  spotify 
+	  firefox 
+	  eclipses.eclipse-sdk
+    ];
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Nordic";
+      package = pkgs.nordic;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme = "gtk";
+    style.name = "gtk";
+  };
+
+  programs.watson = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
 }
