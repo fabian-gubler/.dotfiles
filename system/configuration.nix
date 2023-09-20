d7d5d0d42773c8bfe700747c55853e5c5aa6641a
Fabian Gubler <fabian.gubler@hotmail.com>
2023-09-20 10:54:09 +0200

    automated update

--- system/configuration.nix
+++ system/configuration.nix
@@ -84,6 +84,18 @@ in
 
   time.timeZone = "Europe/Zurich";
 
+  # Databases
+
+  # ...
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "mydatabase" ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
+  };
+
   # TODO: System-wide GTK Theme
   qt = {
     enable = true;
