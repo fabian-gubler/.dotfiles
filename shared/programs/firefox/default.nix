a819903853041cdaf9b758891a9b1bd90ac38d05
Fabian Gubler <fabian.gubler@hotmail.com>
2023-11-03 15:07:52 +0100

    manual update

--- shared/programs/firefox/default.nix
+++ shared/programs/firefox/default.nix
@@ -13,26 +13,27 @@
   programs.firefox = {
     enable = true;
     profiles.default = {
-      search.engines = {
-        "Nix Packages" = {
-          urls = [{
-            template = "https://search.nixos.org/packages";
-            params = [
-              { name = "type"; value = "packages"; }
-              { name = "query"; value = "{searchTerms}"; }
-            ];
-          }];
-
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "np" ];
-        };
-        "YouTube" = {
-          urls = [{
-            template = "https://www.youtube.com/results?search_query={searchTerms}";
-          }];
-          definedAliases = [ "y" ];
-        };
-      };
+      # search.engines = {
+		# TODO: Problems rebuilding
+      #   "Nix Packages" = {
+      #     urls = [{
+      #       template = "https://search.nixos.org/packages";
+      #       params = [
+      #         { name = "type"; value = "packages"; }
+      #         { name = "query"; value = "{searchTerms}"; }
+      #       ];
+      #     }];
+      #
+      #     icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      #     definedAliases = [ "np" ];
+      #   };
+      #   "YouTube" = {
+      #     urls = [{
+      #       template = "https://www.youtube.com/results?search_query={searchTerms}";
+      #     }];
+      #     definedAliases = [ "y" ];
+      #   };
+      # };
       settings = {
         "services.sync.prefs.sync.browser.uiCustomization.state" = true;
         "browser.aboutConfig.showWarning" = false;
