dcd51d28e5e67822c42898927e25ba0e7ab59979
Fabian Gubler <fabian.gubler@hotmail.com>
2023-11-03 19:42:12 +0100

    automated update

--- shared/files/default.nix
+++ shared/files/default.nix
@@ -16,7 +16,6 @@ let
   homeFilesToLink = {
     # TODO: custom
     ".local/share/fonts" = ./fonts; # TODO: write nix package for sf font or find alternative font
-    # ".xprofile" = ./xorg/.xprofile; # TODO: autostart declare in home-manager (find a way)
     ".xinitrc" = ./xorg/.xinitrc; # TODO: is there a nix way?
 
   };
@@ -24,22 +23,21 @@ let
   # Function to help map attrs for symlinking home.file, xdg.configFile
   toSource = configDirName: dotfilesPath: { source = dotfilesPath; };
 
-  # Assuming you have a `pkgs` attribute available
-  home.file.".xprofile".source = pkgs.writeShellScript "xprofile" ''
-    #!/bin/sh
-
-    # startup
-    ${pkgs.hsetroot}/bin/hsetroot -solid "#252A34" &
-    ${pkgs.xbindkeys}/bin/xbindkeys &
-    ${pkgs.clipmenu}/bin/clipmenud &
-    ${pkgs.spice-vdagent}/bin/spice-vdagent &
-  '';
 
 in
 {
 
   # Symlink files under ~, e.g. ~/.xprofile
-  home.file = pkgs.lib.attrsets.mapAttrs toSource homeFilesToLink;
+  home.file = pkgs.lib.attrsets.mapAttrs toSource homeFilesToLink // {
+    # Add custom .xprofile
+    ".xprofile".source = pkgs.writeShellScript "xprofile" ''
+      ${pkgs.hsetroot}/bin/hsetroot -solid "#252A34" &
+      ${pkgs.xbindkeys}/bin/xbindkeys &
+      ${pkgs.clipmenu}/bin/clipmenud &
+      ${pkgs.spice-vdagent}/bin/spice-vdagent &
+    '';
+
+  };
 
   # Symlink files under ~/.config, e.g. ~/.config/alacritty/alacritty.yml
   xdg.configFile = pkgs.lib.attrsets.mapAttrs toSource configFilesToLink;
