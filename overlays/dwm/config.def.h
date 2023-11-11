5e88e33318467efed5a7c8ece382a9080e497a3b
Fabian Gubler <fabian.gubler@hotmail.com>
2023-11-12 00:22:16 +0100

    change termcmd to alacritty

--- overlays/dwm/config.def.h
+++ overlays/dwm/config.def.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"dmenu_run -i", NULL };
-static const char *termcmd[] = { "kitty", NULL };
+static const char *termcmd[] = { "alacritty", NULL };
 
 #include "movestack.c"
 
