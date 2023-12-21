ff8895830a679c13502c7d331b23f5c41a925f51
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-21 11:19:39 +0100

    enhance(eza): add comments of enabled Aliases

--- shared/programs/eza/default.nix
+++ shared/programs/eza/default.nix
@@ -2,6 +2,13 @@
   programs.eza = {
     enable = true;
     enableAliases = true;
+
+  # 'ls': Basic file listing
+  # 'll': Detailed file listing
+  # 'la': List all files, including hidden
+  # 'lt': Display directory tree
+  # 'lla': Detailed list, all files
+
     extraOptions = [
       "--group-directories-first"
     ];
