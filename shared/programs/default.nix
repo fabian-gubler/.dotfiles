4f0b1edd86ee2743a108a20a7a4e763eadb916f0
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-21 11:16:19 +0100

    feat(program): add eza with config

--- shared/programs/default.nix
+++ shared/programs/default.nix
@@ -14,6 +14,7 @@
     ./yt-dlp
     ./zsh
     ./calendar
+    ./eza
     # ./vscode
 	./firefox
   ];
---
+++ shared/programs/eza/default.nix
@@ -0,0 +1,9 @@
+{
+  programs.eza = {
+    enable = true;
+    enableAliases = true;
+    extraOptions = [
+      "--group-directories-first"
+    ];
+  };
+}
