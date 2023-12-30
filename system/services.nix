eca16957e9fc0ddc11c9a9b01131d53d0d18524d
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-30 14:56:57 +0100

    refactor: remove unused dbs

--- system/services.nix
+++ system/services.nix
@@ -23,21 +23,6 @@ in
   services.atd.enable = true;
   services.flatpak.enable = false;
 
-  # services.mongodb.enable = true;
-
-  # services.postgresql = {
-  #   enable = true;
-  #   # package = pkgs.postgresql_15; # current 15.4
-  #   ensureDatabases = [ "mydatabase" ];
-  #   authentication = pkgs.lib.mkOverride 10 ''
-  #     local all all              trust
-  #     host  all all 127.0.0.1/32 trust
-  #     host  all all ::1/128      trust
-  #   '';
-  #   enableTCPIP = true;
-  #   # port = 5432;
-  # };
-
   # misc applications
   services.jellyfin.enable = true;
 
