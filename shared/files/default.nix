5512164564c27c25588671ff2270952802f90b06
Fabian Gubler <fabian.gubler@hotmail.com>
2024-04-11 22:50:15 +0200

    refactor: clean unused files

--- shared/files/default.nix
+++ shared/files/default.nix
@@ -3,18 +3,11 @@
 let
   configFilesToLink = {
     # TODO: custom
-    "dunst" = ./dunst; # TODO: create custom service
     "lazygit/config.yml" = ./lazygit/config.yml; # TODO: create custom service
 
     # Leave it
     "mutt" = ./mutt; # TODO: declarable in home-manager
     "mpv" = ./mpv; # TODO: declarable in home-manager
-  };
-
-  homeFilesToLink = {
-    # TODO: custom
-    ".xinitrc" = ./xorg/.xinitrc; # TODO: is there a nix way?
-
   };
 
   # Function to help map attrs for symlinking home.file, xdg.configFile
@@ -23,18 +16,6 @@ let
 
 in
 {
-
-  # Symlink files under ~, e.g. ~/.xprofile
-  home.file = pkgs.lib.attrsets.mapAttrs toSource homeFilesToLink // {
-    # Add custom .xprofile
-    ".xprofile".source = pkgs.writeShellScript "xprofile" ''
-      ${pkgs.xorg.xsetroot}/bin/xsetroot -solid "#252A34" &
-      ${pkgs.xbindkeys}/bin/xbindkeys &
-      ${pkgs.clipmenu}/bin/clipmenud &
-      ${pkgs.spice-vdagent}/bin/spice-vdagent &
-    '';
-
-  };
 
   # TODO: Delete original files before symlinking
 
