9ec8f1086431f6c2eae7f6b5a1a78fe9a6941a63
Fabian Gubler <fabian.gubler@hotmail.com>
2024-04-13 13:15:47 +0200

    fix: rbw patch

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,6 +15,13 @@
   # Shared 
   modifications = final: prev: {
     dmenu = prev.dmenu.overrideAttrs (oldAttrs: { src = ./dmenu; });
+    rbw = prev.rbw.overrideAttrs (oldAttrs: {
+      patches =
+        oldAttrs.patches
+        ++ [
+          ./rbw-patch.txt
+        ];
+    });
     # eclipse = prev.exclipse.overrideAttrs (oldAttrs: rec {
     #   eclipse = eclipse-platform;
     # });
