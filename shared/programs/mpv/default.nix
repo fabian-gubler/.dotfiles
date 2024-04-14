33e77f8492294d0ceeb1485795462b9e23161a0a
Fabian Gubler <fabian.gubler@hotmail.com>
2024-04-14 19:10:35 +0200

    feat(mpv): add config

--- shared/programs/mpv/default.nix
+++ shared/programs/mpv/default.nix
@@ -10,9 +10,34 @@
       mpvScripts.quality-menu
     ];
     config = {
-        osd-bar = "no";
-        border = "no";
-
+      osd-bar = false;
+      border = false;
+      gpu-context = "wayland";
+      slang = "en";
+      save-position-on-quit = true;
+    };
+    bindings = {
+      tab = "script-binding uosc/toggle-ui";
+      ENTER = "script-binding uosc/menu";
+      DOWN = "add volume -5";
+      UP = "add volume 5";
+      S = "script-binding uosc/stream-quality";
+      C = "script-binding uosc/subtitles";
+      O = "script-binding uosc/open-file";
+      A = "script-binding uosc/audio";
+      P = "script-binding uosc/chapters";
+      L = "script-binding uosc/playlist";
+      t = "script-message-to seek_to toggle-seeker";
+      "0" = "seek 0  absolute-percent";
+      "1" = "seek 10 absolute-percent";
+      "2" = "seek 20 absolute-percent";
+      "3" = "seek 30 absolute-percent";
+      "4" = "seek 40 absolute-percent";
+      "5" = "seek 50 absolute-percent ";
+      "6" = "seek 60 absolute-percent ";
+      "7" = "seek 70 absolute-percent ";
+      "8" = "seek 80 absolute-percent ";
+      "9" = "seek 90 absolute-percent ";
     };
   };
 
