8b09275bdfd6eb721150f055a3a5a71d0a180e30
Fabian Gubler <fabian.gubler@hotmail.com>
2024-04-16 03:30:55 +0200

    feat(zsa): add hardware

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -39,4 +39,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.keyboard.zsa.enable = true;
 }
