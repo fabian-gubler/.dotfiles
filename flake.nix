4d041730bb94de7f66ef63cfca419e607c702eba
Fabian Gubler <fabian.gubler@hotmail.com>
2024-12-10 00:29:17 +0700

    update system

--- flake.nix
+++ flake.nix
@@ -5,12 +5,12 @@
 
   inputs = {
     #	nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
