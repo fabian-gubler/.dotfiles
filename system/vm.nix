8ae4219259251a90aba6b6c803723e316345c3f4
Fabian Gubler <fabian.gubler@hotmail.com>
2025-02-11 11:23:54 -0500

    add vm.nix

---
+++ system/vm.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+
+  # Enable dconf (System Management Tool)
+  programs.dconf.enable = true;
+
+  # Install necessary packages
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    spice spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+    gnome.adwaita-icon-theme
+  ];
+
+  # Manage the virtualisation services
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
+
+}
+
+
+ #  programs.virt-manager.enable = true;
+ #  users.groups.libvirtd.members = ["fabian"];
+ #  virtualisation.libvirtd.enable = true;
+ # virtualisation.spiceUSBRedirection.enable = true;
+ #
+ # virtualisation.libvirtd.qemu.swtpm.enable = true;
+
